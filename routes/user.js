469cf5a051387e2994b8e6e72c701d7878455862
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-03 14:10:47 +0545

    Api

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const fileUpload = require("express-fileupload")
+const cloudinary = require("cloudinary").v2;
+
+require('dotenv').config()
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 router.post("/signup", async (req, res) => {
-  console.log((req));
+ try{
+const uploadPhoto=  await cloudinary.uploader.upload(req.files.photo.tempFilePath);
+  console.log(uploadPhoto);
   
-  res.status(200).json({
-    msg: "Signup",
-  });
-});
+ }
+   catch (err) {
+    console.log("error");
+    res.status(400).json({
+      error:err
+    })
+    
+  }
+}); 
 
 module.exports = router;
