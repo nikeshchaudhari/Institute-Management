843c0eb8735f08bea5985595f66b99ef984adc56
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-03 18:22:05 +0545

    Api

---
+++ routes/courses.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const Course = require("../models/Courses");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary").v2;
+const checkAuth = require("../middleware/checkAuth");
+
+require("dotenv").config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+router.post("/add-course", checkAuth, async (req, res) => {
+  try {
+    const token = await req.headers.authorization.split(" ")[1];
+    const verifyToken = await jwt.verify(token,'secret key')
+    const uploadPhoto = await cloudinary.uploader.upload(
+      req.files.image.tempFilePath
+    );
+    console.log(uploadPhoto);
+    
+    const courseInsert = await new Course({
+      _id: new mongoose.Types.ObjectId(),
+      courseName: req.body.courseName,
+      price: req.body.price,
+      description: req.body.description,
+      startDate:req.body.startDate,
+      endDate: req.body.endDate,
+      uId:verifyToken.uId,
+      imageId:uploadPhoto.public_id,  
+      imageUrl: uploadPhoto.secure_url
+    });
+const data = await courseInsert.save()
+console.log(data);
+res.status(200).json({
+    data:data
+})
+
+  } catch (err) {
+    console.log("error");
+    res.status(400).json({
+        error:err
+    });
+  }
+});
+
+module.exports = router;
