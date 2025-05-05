0e3c5a9aec5b86f4d9a62496365d07df3a19a43a
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-05 13:39:26 +0545

    Frontend

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "../components/style.css";
+import axios from "axios";
+
+import { toast } from 'react-toastify';
 
 const Signup = () => {
   const [fullName, setfullName] = useState("");
@@ -7,18 +10,36 @@ const Signup = () => {
   const [phone, setPhone] = useState("");
   const [password, setpassword] = useState("");
   const [image, setImage] = useState(null);
-  const [imageUrl,setImageUrl]=useState(' ');
+  const [imageUrl, setImageUrl] = useState(" ");
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(fullName, email, phone, password,image);
+    const formData = new FormData();
+    formData.append("fullName", fullName);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("phone", phone);
+    formData.append("image", image);
+
+    axios
+      .post("http://localhost:9500/user/signup", formData)
+      // console.log("Data............");
+      .then((res) => {
+        toast.success("Successfully...")
+        // console.log("Succcess");
+      })
+      .catch((err) => {
+        toast.error("something is wrong...")
+        console.log(err);
+      });
+    // console.log(fullName, email, phone, password,image);
   };
 
-  const fileHandler =(e)=>{
-    setImage(e.target.files[0])
-    setImageUrl(URL.createObjectURL(e.target.files[0]))
-  }
-  
+  const fileHandler = (e) => {
+    setImage(e.target.files[0]);
+    setImageUrl(URL.createObjectURL(e.target.files[0]));
+  };
+
   return (
     <div className="signup-wrapper">
       <div className="signup-box">
@@ -31,6 +52,7 @@ const Signup = () => {
           <h2 className="right-header">Create Your Account ?</h2>
           <form onSubmit={submitHandler} className="signup-form">
             <input
+              required
               onChange={(e) => {
                 setfullName(e.target.value);
               }}
@@ -38,6 +60,7 @@ const Signup = () => {
               placeholder="Institute Name "
             ></input>
             <input
+              required
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
@@ -45,6 +68,7 @@ const Signup = () => {
               placeholder="Emali "
             ></input>
             <input
+              required
               onChange={(e) => {
                 setpassword(e.target.value);
               }}
@@ -52,15 +76,26 @@ const Signup = () => {
               placeholder="Password "
             ></input>
             <input
+              required
               onChange={(e) => {
                 setPhone(e.target.value);
               }}
               type="number"
               placeholder="phone "
             ></input>
-            <input onChange={fileHandler}type="file" placeholder="file "></input>
-            <img alt="imageUrl" src={imageUrl} width="100px"/>
-            <button type="submit">Submit</button>
+            <input
+              required
+              onChange={fileHandler}
+              type="file"
+              placeholder="file "
+            />
+            {imageUrl && <img src={imageUrl} width="100px" />}
+
+            <button type="submit">
+            <i class="fa-solid fa-circle-notch fa-spin"></i>
+
+            Submit
+            </button>
           </form>
         </div>
       </div>
