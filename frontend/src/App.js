725e119d438d9a42c4ecfd70dcb2972bf107de3c
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-05 23:41:09 +0545

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,21 @@ import Signup from "./components/Signup";
 import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import Allcourse from "./components/Allcourse";
 
 const App = () => {
   const router = createBrowserRouter([
     { path: "", Component: Login },
     { path: "login", Component: Login },
     { path: "signup", Component: Signup },
-    { path: "dashboard", Component: Dashboard },
+    { path: "dashboard", Component: Dashboard,children:[
+      {path:"home",Component:Home},
+      {path:"allcourse",Component:Allcourse},
+      
+    
+
+    ] },
   ]);
   return (
     <div>
