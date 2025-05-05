afc4b44e1b268b89844eccf2c5443425ac54c69a
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-05 23:40:27 +0545

    Frontend

--- frontend/src/components/Sidenav.js
+++ frontend/src/components/Sidenav.js
@@ -28,16 +28,16 @@ const Sidenav = () => {
         <h2 className="title">ABC Institute</h2>
       </div>
       <div className="menu-wrapper">
-        <Link className="menu-link">
+        <Link className="menu-link"  to={"/home"}> 
           <span><FaHome /></span>
           Home
         </Link>
-        <Link className="menu-link"> <span><FaBookReader /></span> All Course</Link>
-        <Link className="menu-link"> <span><MdOutlinePlaylistAddCheckCircle /></span> Add Courses </Link>
-        <Link className="menu-link"><span><IoMdPersonAdd /></span> Add Students</Link>
-        <Link className="menu-link"> <span><PiStudent /></span>All Students</Link>
-        <Link className="menu-link"><span><GiTakeMyMoney /></span>Collect Fee</Link>
-        <Link className="menu-link"><span><FaHistory /></span>Payment History</Link>
+        <Link className="menu-link" to={"/allcourse"}> <span><FaBookReader /></span> All Course</Link>
+        <Link className="menu-link" to={"/addcourse"}> <span><MdOutlinePlaylistAddCheckCircle /></span> Add Courses </Link>
+        <Link className="menu-link" to={"/addstudent"}><span><IoMdPersonAdd /></span> Add Students</Link>
+        <Link className="menu-link" to={"allstudent"}> <span><PiStudent /></span>All Students</Link>
+        <Link className="menu-link" to={"/collect"}><span><GiTakeMyMoney /></span>Collect Fee</Link>
+        <Link className="menu-link" to={"/payhistory"}><span><FaHistory /></span>Payment History</Link>
       </div>
     </div>
   );
