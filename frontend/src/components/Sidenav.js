24e0c9d2674c806e82a44373e94215cdefc8fb4e
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-06 14:10:04 +0545

    Frontend

--- frontend/src/components/Sidenav.js
+++ frontend/src/components/Sidenav.js
@@ -8,13 +8,7 @@ import { PiStudent } from "react-icons/pi";
 import { GiTakeMyMoney } from "react-icons/gi";
 import { FaHistory } from "react-icons/fa";
 
-
-
 import { MdOutlinePlaylistAddCheckCircle } from "react-icons/md";
-
-
-
-
 
 const Sidenav = () => {
   return (
@@ -22,22 +16,57 @@ const Sidenav = () => {
       <div className="side-nav">
         <img
           className="profile-image"
-          src={require("../components/assets/pexels-photo-15092999.webp")}
+          src={require("../assets/pexels-photo-15092999.webp")}
           width="50px"
         />
         <h2 className="title">ABC Institute</h2>
       </div>
       <div className="menu-wrapper">
-        <Link className="menu-link"  to={"/home"}> 
-          <span><FaHome /></span>
+        <Link className="menu-link" to={"/home"}>
+          <span>
+            <FaHome />
+          </span>
           Home
         </Link>
-        <Link className="menu-link" to={"/allcourse"}> <span><FaBookReader /></span> All Course</Link>
-        <Link className="menu-link" to={"/addcourse"}> <span><MdOutlinePlaylistAddCheckCircle /></span> Add Courses </Link>
-        <Link className="menu-link" to={"/addstudent"}><span><IoMdPersonAdd /></span> Add Students</Link>
-        <Link className="menu-link" to={"allstudent"}> <span><PiStudent /></span>All Students</Link>
-        <Link className="menu-link" to={"/collect"}><span><GiTakeMyMoney /></span>Collect Fee</Link>
-        <Link className="menu-link" to={"/payhistory"}><span><FaHistory /></span>Payment History</Link>
+        <Link className="menu-link" to={"/allcourse"}>
+          {" "}
+          <span>
+            <FaBookReader />
+          </span>{" "}
+          All Course
+        </Link>
+        <Link className="menu-link" to={"/addcourse"}>
+          {" "}
+          <span>
+            <MdOutlinePlaylistAddCheckCircle />
+          </span>{" "}
+          Add Courses{" "}
+        </Link>
+        <Link className="menu-link" to={"/addstudent"}>
+          <span>
+            <IoMdPersonAdd />
+          </span>{" "}
+          Add Students
+        </Link>
+        <Link className="menu-link" to={"/allstudent"}>
+          {" "}
+          <span>
+            <PiStudent />
+          </span>
+          All Students
+        </Link>
+        <Link className="menu-link" to={"/fee"}>
+          <span>
+            <GiTakeMyMoney />
+          </span>
+          Collect Fee
+        </Link>
+        <Link className="menu-link" to={"/payhistory"}>
+          <span>
+            <FaHistory />
+          </span>
+          Payment History
+        </Link>
       </div>
     </div>
   );
