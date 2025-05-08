cdeba87ee7dec436af6da27ecc67040089e93b0b
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-08 20:56:54 +0545

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,14 @@ import Signup from "./components/Signup";
 import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import AllCourse from './components/AllCourse'
+import AllStudent from './components/AllStudent'
 import Home from "./components/Home";
-import Allcourse from "./components/Allcourse";
+import AddCourse from "./components/AddCourse";
+import AddStudent from "./components/AddStudent";
+
+import Fee from "./components/Fee";
+import PayHistory from "./components/PayHistory";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -14,8 +20,15 @@ const App = () => {
     { path: "login", Component: Login },
     { path: "signup", Component: Signup },
     { path: "dashboard", Component: Dashboard,children:[
+      {path:"",Component:Home},
       {path:"home",Component:Home},
-      {path:"allcourse",Component:Allcourse},
+      {path:"addcourse",Component:AddCourse},
+      {path:"allcourse",Component:AllCourse},
+      {path:"addstudent",Component:AddStudent},
+      {path:"allstudent",Component:AllStudent},
+      {path:"fee",Component:Fee},
+      {path:"paymethistory",Component:PayHistory}
+     
       
     
 
