47ae55a8e0e0fa8bbbe702d18f3247aa23da69de
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-17 16:04:15 +0545

    update

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express();
+const userRoute =require('./routes/user');
+const courseRoute = require('./routes/courses');
+const stdRoute = require('./routes/student')
+const feeRoute=require('./routes/fee')
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors")
+
+require('dotenv').config()
+const dbConnect = async()=>{
+try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database Connect....");
+    
+}
+catch(err){
+    console.log("Error");
+    res.status(400).json({
+        error:err
+    })
+    
+}
+}
+dbConnect();
+
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles:true,
+    // tempFileDir:'/tmp/'
+}))
+app.use('/user',userRoute)
+app.use('/course',courseRoute)
+app.use('/student',stdRoute)
+app.use('/pay',feeRoute)
+
+app.use('/',(req,res)=>{
+res.status(404).json({
+    msg:"bad request"
+})
+})
+
+
+module.exports=app;
